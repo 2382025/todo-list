84da13ba3dc48942d541648fef40909e8be828c0
2382025 <2382025@unai.edu>
2024-11-21 20:50:42 +0700

    handson 10

--- todoapp/Main.java
+++ todoapp/Main.java
@@ -1,27 +1,23 @@
-import todoapp.config.Database;
-import todoapp.repositories.TodoListRepository;
-import todoapp.repositories.TodoListRepositoryDbImpl;
-import todoapp.services.TodoListService;
-import todoapp.services.TodoListServiceImpl;
-import todoapp.views.TodoListTerminalViewImpl;
-import todoapp.views.TodoListView;
-import org.springframework.stereotype.Component;
+package todoapp;
 
-import javax.xml.crypto.Data;
-import java.beans.BeanProperty;
+import todoapp.config.Database;
+import todoapp.views.TodoListView;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @ComponentScan(basePackages = "todoapp")
 public class Main {
     public static void main(String[] args) {
-    ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-    TodoListView todoListView = context,getBean(TodoListView.class);
-    todoListView.run();
-
-
+        ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        TodoListView todoListView = context.getBean(TodoListView.class);
+        todoListView.run();
     }
 
     @Bean
-    Database database(){
+    public Database database() {
         Database database = new Database("databaseku", "root", "", "localhost", "3306");
         database.setup();
         return database;
